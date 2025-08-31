8d9cbc96afbaa1ba8fcbabca018e7ade7f23f73f
Sachithra madusanka <Bloodxmd6@gmail.com>
2025-08-31 09:21:24 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  SESSION_ID: process.env.SESSION_ID || "Caseyrhodes~mk5GjD7R#n1QiyOz73Rn3PPdy6hzM8YhAS0PMvUJzTA1XNIe9ih4",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
