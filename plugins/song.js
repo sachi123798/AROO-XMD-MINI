9d775550571cc70da78b130606bf9304f9ce7ccd
Sachithra madusanka <Bloodxmd6@gmail.com>
2025-09-23 09:23:02 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -46,7 +46,7 @@ const song = async (m, gss) => {
         {
           video: { url: download_url },
           mimetype: "video/mp4",
-          caption: `> *${title}*\n\nPowered By Caseyrhodes 💜`,
+          caption: `> *${title}*\n\nPowered By Ashen md 💜`,
         },
         { quoted: m }
       );
